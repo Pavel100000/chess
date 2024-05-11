efd906fbd297aa0f5a02d3d62d38a01d9d255c89
Pavel100000 <utkin.pv@phystech.edu>
2024-05-11 17:16:23 +0300

    dota3

--- gui/gui.c
+++ gui/gui.c
@@ -5,50 +5,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define WINDOW_WIDTH 900
-#define WINDOW_HEIGHT 900
-#define TILE_SIZE 100
+#define WINDOW_WIDTH 450
+#define WINDOW_HEIGHT 450
+#define TILE_SIZE 50
 #define BOARD_SIZE 8
+
+// b square rgb(181,136,99)
+// w square rgb(240,217,181)
 
 void DrawChessboard(SDL_Renderer* renderer) {
     SDL_Rect rect;
     rect.w = TILE_SIZE;
     rect.h = TILE_SIZE;
 
-    for (int row = 0; row < BOARD_SIZE + 1; row++) {
-        for (int col = 0; col < BOARD_SIZE + 1; col++) {
+    for (int row = 0; row < BOARD_SIZE; row++) {
+        for (int col = 0; col < BOARD_SIZE; col++) {
             rect.x = col * TILE_SIZE;
             rect.y = row * TILE_SIZE;
 
-            if ((row == 0 || col == 0) || ((row + col) % 2 == 0)) {
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // Белый цвет
+            if ((row + col) % 2 == 0) {
+                SDL_SetRenderDrawColor(renderer, 240, 217, 181, 255);  // Белый цвет
             }
             else {
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  // Черный цвет
+                SDL_SetRenderDrawColor(renderer, 181, 136, 99, 255);  // Черный цвет
             }
 
             SDL_RenderFillRect(renderer, &rect);
-
-            if (row == 0 && col > 0) {
-                char label = 'a' + col - 1;
-                SDL_Color text_color = { 0, 0, 0, 255 };
-                SDL_Surface* surface = TTF_RenderText_Solid(NULL, &label, text_color);
-                SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-                SDL_Rect dstrect = { col * TILE_SIZE, 0, TILE_SIZE, TILE_SIZE };
-                SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-                SDL_DestroyTexture(texture);
-                SDL_FreeSurface(surface);
-            }
-            else if (col == 0 && row > 0) {
-                char label[2] = { (char)(BOARD_SIZE - row + '0' + 1), '\0' };
-                SDL_Color text_color = { 0, 0, 0, 255 };
-                SDL_Surface* surface = TTF_RenderText_Solid(NULL, label, text_color);
-                SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-                SDL_Rect dstrect = { 0, row * TILE_SIZE, TILE_SIZE, TILE_SIZE };
-                SDL_RenderCopy(renderer, texture, NULL, &dstrect);
-                SDL_DestroyTexture(texture);
-                SDL_FreeSurface(surface);
-            }
         }
     }
 }
@@ -71,6 +53,11 @@ int WinMain(int argc, char* argv[]) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = true;
+            }
+            if (event.button.button == SDL_BUTTON_LEFT) {
+                int x = event.button.x;
+                int y = event.button.y;
+                printf("%d %d", x, y);
             }
         }
 
